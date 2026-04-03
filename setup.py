c1a5eb0f396390511a8c1e953c4883cbc0879ddd
thddmsthf06-del <thddmsthf06@gmail.com>
2026-04-03 19:40:19 +0900

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kbo',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of your package',
+    url='https://github.com/thddmsthf06-del/kbo',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
